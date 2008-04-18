e7d771b284c60965b7a8a6ed58ed03c3f5e71666
jaguar.scratch <jaguar.scratch@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-04-18 01:30:17 +0000

    añadidas clases AdministradoraDeUsuariosAdministradoraSolicitudesExamen y sus métodos sin implementar

---
+++ trunk/Business Layer/src/com/liceoval/businesslayer/control/AdministradoraSolicitudesExamen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liceoval.businesslayer.control;
+
+import com.liceoval.businesslayer.entities.Examen;
+import com.liceoval.businesslayer.entities.Registro;
+
+/**
+ *
+ * @author Jorge
+ */
+public class AdministradoraSolicitudesExamen {
+    
+    public void SolicitarExamen(Examen examenSolicitado){
+        
+        Registro registroDeEstudiante;
+        //traer registro
+    }
+    
+    
+
+}
