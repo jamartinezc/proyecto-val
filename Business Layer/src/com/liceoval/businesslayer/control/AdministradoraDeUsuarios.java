f1903646e4b80ea661582fd06be6feb038e95c9f
jaguar.scratch <jaguar.scratch@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-04-18 01:30:17 +0000

    añadidas clases AdministradoraDeUsuariosAdministradoraSolicitudesExamen y sus métodos sin implementar

---
+++ Business Layer/src/com/liceoval/businesslayer/control/AdministradoraDeUsuarios.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.liceoval.businesslayer.control;
+
+import com.liceoval.businesslayer.entities.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Jorge
+ */
+public class AdministradoraDeUsuarios {
+    public List<Usuario> solicitarListaDeUsuarios(){
+        
+        return null;
+    }
+    
+    public void crearUsuario(Usuario usuarioNuevo){
+        
+    }
+    
+    public void modificarUsuario(Usuario usuario){
+        
+    }
+    
+    public void eliminarUsuario(Usuario usuario){
+        
+    }
+}
