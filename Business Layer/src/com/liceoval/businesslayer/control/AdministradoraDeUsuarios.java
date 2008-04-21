58440a0955007b7bad683ed6d627993fa4bd32ef
jaguar.scratch <jaguar.scratch@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-04-21 02:06:27 +0000

    implementacion inicial de AdministradoraDeusuarios.eliminarUsuario, AdministradoraDeusuarios.modificarUsuario, AdministradoraDeusuarios.crearUsuario., se borraron algunos comentarios y se agregó javaDoc para AdministradoraDeUsuarios

--- Business Layer/src/com/liceoval/businesslayer/control/AdministradoraDeUsuarios.java
+++ Business Layer/src/com/liceoval/businesslayer/control/AdministradoraDeUsuarios.java
@@ -1,26 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.liceoval.businesslayer.control;
 
 import com.liceoval.businesslayer.entities.Usuario;
 import java.util.List;
 
 import CRUD.Crud;
+import java.util.LinkedList;
+
 /**
  *
  * @author Jorge
  */
+
 public class AdministradoraDeUsuarios {
     
+    /**
+     * 
+     * @return una lista List&lt;Usuario&gt; conteniendo los usuarios del
+     * sistema registrados en al base de datos.
+     */
     public List<Usuario> solicitarListaDeUsuarios(){
         Crud driverDB;
         driverDB = new Crud();
         
         List<VO.Usuario> listaTemp = driverDB.listaUsuarios();
-        List<Usuario> listaUsuarios = null;
+        List<Usuario> listaUsuarios = new LinkedList();
         
         for(int i=0;i<listaTemp.size();i++){
             VO.Usuario usuario = listaTemp.get(i);
@@ -38,17 +41,43 @@ public class AdministradoraDeUsuarios {
         return listaUsuarios;
     }
     
-    public void crearUsuario(String nombres, String apellidos, String login, String clave){
+    /**
+     * 
+     * @param nombres String con los nombres del usuario
+     * @param apellidos String con los apellidos del usuario
+     * @param login String con el login de usuario
+     * @param clave String con la password del usuario
+     * @return true
+     * @throws java.lang.Exception
+     */
+    public boolean crearUsuario(String nombres, String apellidos, String login, String clave){
         Crud driverDB;
         driverDB = new Crud();
         
         if(nombres!=null && apellidos!=null && login!=null && clave!=null){
-            driverDB.crearUsuario(nombres, apellidos, login, clave);
+            VO.Usuario usuarioCreado = driverDB.crearUsuario(nombres, apellidos, login, clave);
+            if(usuarioCreado != null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
         }
         
     }
     
-    public void modificarUsuario(Usuario usuario){
+    /**
+     * 
+     * @param usuario objeto usuairo con el ID de usuario y con los datos como
+     * se quiere que estén despues de la modificacion.
+     * @return <p>true si se modificó satisfacroriamente </p>
+     * <p>false si hubo un error y no se pudo modificar
+     * @throws java.lang.Exception si el usuario no existe.
+     */
+    public boolean modificarUsuario(Usuario usuario){
         Crud driverDB;
         driverDB = new Crud();
         
@@ -59,18 +88,38 @@ public class AdministradoraDeUsuarios {
             String login = usuario.getLogin();
             String clave = usuario.getPassword().toString();
             
-            driverDB.actualizarUsuario(idUsuario, nombres, apellidos, login, clave);
+            VO.Usuario usuarioModificado = driverDB.actualizarUsuario(idUsuario, nombres, apellidos, login, clave);
+            if(usuarioModificado != null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
         }
         
     }
     
-    public void eliminarUsuario(Usuario usuario){
+    /**
+     * 
+     * @param usuario objeto usuario con el ID de usuario del usuario que se
+     * quiere eliminar.
+     * @return <p>True: si se ha eliminado satisfactoriamente el usuario</p>
+     * <p>False: si no se ha podido eleminar el usuario</p>
+     * @throws java.lang.Exception si el usuario no existe.
+     */
+    public boolean eliminarUsuario(Usuario usuario){
         Crud driverDB;
         driverDB = new Crud();
         
         if(usuario != null){
             
-            driverDB.eliminarUsuario( usuario.getIdUsuario() );
+            return driverDB.eliminarUsuario( usuario.getIdUsuario() );
+        }
+        else{
+            return false;
         }
         
     }
