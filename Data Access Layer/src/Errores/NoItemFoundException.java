e43d5f75c1eb3b366458bd0751dc24a6adb778a6
damontic@gmail.com <damontic@gmail.com@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-04-21 01:21:12 +0000

    Crear examen solicitado crea examen solicitado si no se repite exactamente la misma info en alguna fila d ela tabla. Hice otros cambios por ahi pero no recuerdo bien XD, lo importante es q se pueden agregar examenes solicitados. Si lo agrega retorna el examen solicitado, sino retorna null.

---
+++ Data Access Layer/src/Errores/NoItemFoundException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Errores;
+
+/**
+ *
+ * @author David
+ */
+public class NoItemFoundException extends Exception{
+    
+}
