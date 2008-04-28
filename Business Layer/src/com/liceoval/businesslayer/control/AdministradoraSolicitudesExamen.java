4aefc93f04954c405c6242de09a6c43f98919cfe
jaguar.scratch <jaguar.scratch@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-04-28 06:54:43 +0000

    agregado método getSiguienteExamen, que retorna el siguiente examen a presentar de una materia dada

--- Business Layer/src/com/liceoval/businesslayer/control/AdministradoraSolicitudesExamen.java
+++ Business Layer/src/com/liceoval/businesslayer/control/AdministradoraSolicitudesExamen.java
@@ -1,7 +1,10 @@
 package com.liceoval.businesslayer.control;
 
 
+import CRUD.Crud;
 import Errores.NoItemFoundException;
+import Errores.NoPresentableException;
+import Errores.UltimoTemaException;
 import com.liceoval.businesslayer.control.registro.ControladoraDeRegistro;
 import com.liceoval.businesslayer.control.registro.exceptions.EstudianteNoPuedeRegistrarMasExamenesException;
 import com.liceoval.businesslayer.control.registro.exceptions.InsersionDeExamenException;
@@ -9,6 +12,8 @@ import com.liceoval.businesslayer.control.registro.exceptions.NoExisteAnalistaPa
 import com.liceoval.businesslayer.control.registro.exceptions.RegistroNoExisteYNoPuedeSerCreadoException;
 import com.liceoval.businesslayer.control.registro.exceptions.ZonaHorariaIncorrectaException;
 import com.liceoval.businesslayer.entities.Estudiante;
+import com.liceoval.businesslayer.entities.Examen;
+import com.liceoval.businesslayer.entities.entitytranslator.EntityTranslator;
 import com.liceoval.businesslayer.exceptions.InvalidProcedureCallOrArgumentException;
 
 
@@ -18,11 +23,23 @@ import com.liceoval.businesslayer.exceptions.InvalidProcedureCallOrArgumentExcep
  */
 public class AdministradoraSolicitudesExamen {
     
-    public void SolicitarExamen(Estudiante estudiante, int codExamen, int codigoMateria) throws NoItemFoundException, InvalidProcedureCallOrArgumentException, RegistroNoExisteYNoPuedeSerCreadoException, NoExisteAnalistaParaMateriaException, EstudianteNoPuedeRegistrarMasExamenesException, ZonaHorariaIncorrectaException, InsersionDeExamenException{
+    public static void SolicitarExamen(Estudiante estudiante, int codExamen, int codigoMateria) throws NoItemFoundException, InvalidProcedureCallOrArgumentException, RegistroNoExisteYNoPuedeSerCreadoException, NoExisteAnalistaParaMateriaException, EstudianteNoPuedeRegistrarMasExamenesException, ZonaHorariaIncorrectaException, InsersionDeExamenException{
 
         int idEstudiante = estudiante.getCodigo();
         ControladoraDeRegistro.agregarExamen(codExamen, idEstudiante, codigoMateria);
         
     }
     
+    public static Examen getSiguienteExamen(Estudiante estudiante, int codigoMateria) throws UltimoTemaException, NoItemFoundException, NoPresentableException{
+        
+        int idEstudiante = estudiante.getCodigo();
+        Crud driverDB = new Crud();
+        VO.Examen examen = driverDB.getSiguienteExamenDeMateria(codigoMateria, idEstudiante);
+        
+        Examen examenR = EntityTranslator.translateExamen(examen);
+        
+        return examenR;
+        
+    }
+        
 }
