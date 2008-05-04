8d4fe2b605cfc4adeb460b0f9a6637681ec72a7d
damontic@gmail.com <damontic@gmail.com@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-05-04 04:20:12 +0000

    La idea es simplificar el CRUD y dejar en él solamente los métodos de consultas complejas como getSiguienteExamen. Por ahora les mando el Dao de analista, muy pronto tendré los otros.

---
+++ trunk/Data Access Layer/src/DAO/DaoAnalista.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+import Errores.NoItemFoundException;
+import Errores.PosibleDuplicationException;
+import VO.Analista;
+import VO.Usuario;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author David
+ */
+public class DaoAnalista {
+    
+    public static List<Analista> consultarTodos() {   
+        EntityManager em = DaoEntityManagerFactory.getInstance();
+        Query query = em.createNamedQuery("Analista.consultarAnalistas");
+        List<Analista> items = query.getResultList();
+        em.clear();
+        return items;
+    }
+    
+    public static Analista consultarUno(int idAnalista) throws NoItemFoundException{
+        EntityManager em = DaoEntityManagerFactory.getInstance();
+        Query query = em.createNamedQuery("Analista.consultarUnAnalista");
+        query.setParameter("id", idAnalista);
+        try{
+            Analista item = (Analista) query.getSingleResult();
+            em.clear();
+            return item;
+        }
+        catch(NoResultException noResult){
+            em.clear();
+            throw new NoItemFoundException();
+        }
+    }
+    
+    public static Analista eliminar(int idAnalista) throws NoItemFoundException{  
+        EntityManager em = DaoEntityManagerFactory.getInstance();
+        EntityTransaction tx = em.getTransaction();
+        try
+            {
+                tx.begin();
+                    Analista item = em.getReference(Analista.class, idAnalista);
+                    em.remove(item);
+                tx.commit();
+                return item;
+            }
+            catch(EntityNotFoundException noResult){
+                em.clear();
+                throw new NoItemFoundException();
+            }
+            finally
+            {
+                if (tx.isActive())
+                {
+                    tx.rollback();
+                }
+                em.clear();
+            }
+    }
+    
+    public static Analista crear(int idUsuario) throws NoItemFoundException, PosibleDuplicationException{  
+        EntityManager em = DaoEntityManagerFactory.getInstance();
+        EntityTransaction tx = em.getTransaction();
+        try
+            {
+                Usuario usuario = em.getReference(Usuario.class, idUsuario);
+                Query query = em.createNamedQuery("Analista.consultarPorIdUsuario");
+                query.setParameter("id", usuario);
+                
+                if(query.getResultList().size() != 0)
+                {
+                    throw new PosibleDuplicationException();
+                }
+                
+                Analista nuevo = new Analista();
+                nuevo.setIdUsuario(usuario);
+
+                tx.begin();
+                    em.persist(nuevo);
+                tx.commit();   
+
+                em.clear();
+                return (Analista) query.getSingleResult();
+            }
+            catch(NonUniqueResultException error){
+                    throw new PosibleDuplicationException();
+                }
+            catch(EntityNotFoundException noResult){
+                em.clear();
+                throw new NoItemFoundException();
+            }
+            catch(NoResultException noResult){
+                em.clear();
+                throw new NoItemFoundException();
+            }
+            finally
+            {
+                if (tx.isActive())
+                {
+                    tx.rollback();
+                }
+                em.clear();
+            }
+    }
+    
+}
