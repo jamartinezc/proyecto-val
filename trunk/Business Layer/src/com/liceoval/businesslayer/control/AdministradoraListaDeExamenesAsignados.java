2531142f2ad6b7a31523283b34ca181c9b58a1ff
jaguar.scratch <jaguar.scratch@f1b44562-684a-0410-b7cf-b7112635ab22>
2008-06-16 07:25:43 +0000

    actualiza la cantidad de examenes ganados en ExamenMes

--- trunk/Business Layer/src/com/liceoval/businesslayer/control/AdministradoraListaDeExamenesAsignados.java
+++ trunk/Business Layer/src/com/liceoval/businesslayer/control/AdministradoraListaDeExamenesAsignados.java
@@ -6,6 +6,7 @@
 package com.liceoval.businesslayer.control;
 
 import Errores.NoItemFoundException;
+import VO.ExamenMes;
 import com.liceoval.businesslayer.entities.Estado;
 import com.liceoval.businesslayer.entities.Estudiante;
 import com.liceoval.businesslayer.entities.ExamenSolicitado;
@@ -14,6 +15,7 @@ import com.liceoval.businesslayer.entities.entitytranslator.EntityTranslator;
 import com.liceoval.businesslayer.entities.wrappers.ExamenSolicitadoWrapper;
 
 import java.util.*;
+import java.util.LinkedList;
 /**
  *
  * @author Jorge
@@ -30,7 +32,28 @@ public class AdministradoraListaDeExamenesAsignados {
         
         VO.Analista analista = DAO.DaoAnalista.consultarUno(idAnalista);
                     //Collection<VO.ExamenSolicitado> examenesVO = analista.getExamenSolicitadoCollection();
-        Collection<VO.ExamenSolicitado> examenesVO = null;//DAO.DaoAnalista.consultarExamenesSolicitadosPendienteCalificarDeAnalista(idAnalista);
+        List<VO.ExamenSolicitado> examenesVoGanados, examenesVoNP ,examenesVoNE ,examenesVO = new LinkedList<VO.ExamenSolicitado>();
+        try {
+            examenesVoGanados = DAO.DaoExamenSolicitado.examenesSolicitadosDeAnalistaPorEstado(idAnalista, Estado.PENDIENTE_CALIFICAR.getIdEstado());
+        } catch (NoItemFoundException e) {
+            examenesVoGanados = new LinkedList<VO.ExamenSolicitado>();
+        }
+        
+        try {
+            examenesVoNP = DAO.DaoExamenSolicitado.examenesSolicitadosDeAnalistaPorEstado(idAnalista, Estado.NOTA_PENDIENTE.getIdEstado());
+        } catch (NoItemFoundException e) {
+            examenesVoNP = new LinkedList<VO.ExamenSolicitado>();
+        }
+            
+        try {
+            examenesVoNE = DAO.DaoExamenSolicitado.examenesSolicitadosDeAnalistaPorEstado(idAnalista, Estado.NOTA_EXAMEN.getIdEstado());
+        } catch (NoItemFoundException e) {
+            examenesVoNE = new LinkedList<VO.ExamenSolicitado>();
+        }
+            
+            examenesVO.addAll(examenesVoGanados);
+            examenesVO.addAll(examenesVoNP);
+            examenesVO.addAll(examenesVoNE);
         
         VO.Registro registroVO = new VO.Registro();
         Registro registro = new Registro();
@@ -60,23 +83,53 @@ public class AdministradoraListaDeExamenesAsignados {
     public static void guardarNota(int idExamenSolicitado, float nota) throws NoItemFoundException {
         
         DAO.DaoExamenSolicitado.actualizarNotaDeExamenSolicitado(idExamenSolicitado, nota);
-        //TODO actualizar la cantidad de examenes presentados
-        
-        //int[] ganadosPerdidos = DUMMY.DAO.DaoExamenMes.setExamenesPresentadosYganados();
-        
+        //TODO verificar que el examen le haya sido asignado al analista que lo está calificando
+        //DAO.DaoExamenSolicitado.consultarUno(idExamenSolicitado).getIdAnalista().getIdAnalista()
         //Inicializar los valores de nota pendiente y nota examinadero:
         float NPi=8.0f,NPf=8.4f,NEi=8.5f,NEf=8.9f,P=7.9f,G=9.0f;
         
        //inicializar los valores con la base de datos
+       try {
+            NPi = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaPendienteInferior").getValor());
+            NPf = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaPendienteSuperior").getValor());
+            NEi = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaExamenInferior").getValor());
+            NEf = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaExamenSuperior").getValor());
+            P = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaPerdidoSuperior").getValor());
+            G = Float.parseFloat(DAO.DaoVariablesGlobales.consultarUno("NotaGanadoInferior").getValor());
+            //avisar que se cometió un error
+        } catch (NoItemFoundException e) {
+            e.printStackTrace();
+            NPi=8.0f;NPf=8.4f;NEi=8.5f;NEf=8.9f;P=7.9f;G=9.0f;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            NPi=8.0f;NPf=8.4f;NEi=8.5f;NEf=8.9f;P=7.9f;G=9.0f;
+        }
         
         if(nota>=NPi && nota<=NPf){
             DAO.DaoExamenSolicitado.actualizarEstadoDeExamenSolicitado(idExamenSolicitado, Estado.NOTA_PENDIENTE.getIdEstado());
         }else if(nota>=NEi && nota<=NEf){
             DAO.DaoExamenSolicitado.actualizarEstadoDeExamenSolicitado(idExamenSolicitado, Estado.NOTA_EXAMEN.getIdEstado());
-            //TODO actualizar la cantidad de examenes ganados
+            
         }else if(nota>=G){
             DAO.DaoExamenSolicitado.actualizarEstadoDeExamenSolicitado(idExamenSolicitado, Estado.GANADO.getIdEstado());
-            //TODO actualizar la cantidad de examenes ganados
+            VO.ExamenSolicitado examenACalificar = DAO.DaoExamenSolicitado.consultarUno(idExamenSolicitado);
+            int idEstudianteExamen = examenACalificar.getIdRegistro().getIdEstudiante().getIdEstudiante();
+            int idMateriaExamen = examenACalificar.getIdRegistro().getIdMateria().getIdMateria();
+            Calendar fechaExamen = Calendar.getInstance();
+            fechaExamen.setTime(examenACalificar.getFecha());
+            ExamenMes examenMes = null;
+            int ganadosPorEstudiante=0;
+            try {
+                examenMes = DAO.DaoExamenMes.examenesMesDeMateriaDeEstudianteEnMes(idEstudianteExamen, fechaExamen.get(Calendar.MONTH), idMateriaExamen);
+                ganadosPorEstudiante = examenMes.getGanados();
+            } catch (NoItemFoundException e) {
+                System.out.println("no se encontró examenMes con idE: "+idEstudianteExamen+" mes: "+fechaExamen.get(Calendar.MONTH)+" idMateria: "+idMateriaExamen);
+            }
+            try {
+                DAO.DaoExamenMes.actualizarGanadosExamenMes(examenMes.getIdExamenMes(), ganadosPorEstudiante + 1);
+            } catch (NoItemFoundException e) {
+                System.out.println("no se encontro ExamenMes con idMateria: "+idMateriaExamen+" para"+(ganadosPorEstudiante+1));
+            }
         }else if(nota<=P){
             DAO.DaoExamenSolicitado.actualizarEstadoDeExamenSolicitado(idExamenSolicitado, Estado.PERDIDO.getIdEstado());
         }
@@ -84,5 +137,9 @@ public class AdministradoraListaDeExamenesAsignados {
     
     public static void asignarNoPresentado(int idExamenSolicitado) throws NoItemFoundException{
         DAO.DaoExamenSolicitado.actualizarEstadoDeExamenSolicitado(idExamenSolicitado, Estado.NO_PRESENTADO.getIdEstado());
+    }
+    
+    public static boolean validarNota(String nota){
+        return true;
     }
 }
